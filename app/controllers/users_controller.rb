7cc1d178eef5ec147f1bdde1a60eedf1a19d0d85
Alex Zuniga <alexander.zuniga@fitmentgroup.com>
2024-11-18 20:14:08 -0600

    ruby version, sessions, authentications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,55 @@
 class UsersController < ApplicationController
+
+  before_action :require_signin, except: [:new, :create]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user, notice: "Thanks for signing up!" 
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "Thanks for updating!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy!
+    reset_session
+    # OR 
+    # session[:user_id] = nil
+    redirect_to users_url, status: :see_other, alery: "Profile deleted"
+  end
+
+  private
+  def user_params
+    params.require(:user).
+      permit(:name, :email, :password, :password_confirmation, :username)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, presence: true 
+  validates :name, presence: true, length: { minimum: 10, allow_blank: true } 
   validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[A-Z0-9]+\z/i }
+
+  def gravatar_id
+    Digest::MD5::hexdigest(email.downcase)
+  end
 
 end
